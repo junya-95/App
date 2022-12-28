3471cc9dd319b67ee3f7f0ca64b2c0be877e2366
junya <karintou9j5manjuu@gmail.com>
2022-12-28 19:45:47 +0900

    3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +14,9 @@ use App\Http\Controllers\TaskController;
 |
 */
 
-Route::get('/folders/{id}/tasks',[TaskController::class,'index'])->name('tasks.index');
+Route::get('/folders/{id}/tasks',[TaskController::class,'index'])
+    ->name('tasks.index');
+
+Route::get('/folders/create', [FolderController::class,'showCreateForm'])
+    ->name('folders.create');
+Route::post('/folders/create', [FolderController::class,'create']);
