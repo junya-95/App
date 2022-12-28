8e1ec0693592460c200a23cb378c2096294d5390
junya <karintou9j5manjuu@gmail.com>
2022-12-28 19:26:08 +0900

    2

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // すべてのフォルダを取得する
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index')
         ->with([
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(FoldersTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
     }
 }
