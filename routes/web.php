269e691f8d0202a18ea1f1d04f1f06c4842da92e
junya <karintou9j5manjuu@gmail.com>
2022-12-28 20:02:36 +0900

    4

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/folders/{id}/tasks',[TaskController::class,'index'])
 Route::get('/folders/create', [FolderController::class,'showCreateForm'])
     ->name('folders.create');
 Route::post('/folders/create', [FolderController::class,'create']);
+
+Route::get('/folders/{id}/tasks/create', [TaskController::class,'showCreateForm'])
+    ->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [TaskController::class,'create']);
